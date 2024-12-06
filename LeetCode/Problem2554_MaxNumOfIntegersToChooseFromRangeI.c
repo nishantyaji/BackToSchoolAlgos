c7cadac11edab24fd8ff3aa6cfcd231c9b1bb583
nishantyaji <nishantyaji@users.noreply.github.com>
2024-12-06 12:40:51 +0530

    Solution to Leetcode Problem 2554

---
+++ LeetCode/Problem2554_MaxNumOfIntegersToChooseFromRangeI.c
@@ -0,0 +1,19 @@
+int maxCount(int* banned, int bannedSize, int n, int maxSum) {
+    int * flags = (int *)malloc(sizeof(int) * n);
+    memset(flags, 0, n * sizeof(flags[0]));
+    int i = 0;
+    for(i = 0; i < bannedSize; i++) {
+        if(banned[i] <= n) {
+            printf("\n%d - %d", i, banned[i]);
+            flags[banned[i] - 1] = 1;
+        }
+    }
+    int total = 0, count = 0;
+    for(i = 1; i <= n; i++) {
+        if(flags[i-1] == 1) continue;
+        total += i;
+        if(total > maxSum) break;
+        count++;
+    }
+    return count;
+}
