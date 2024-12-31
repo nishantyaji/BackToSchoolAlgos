9156e68f351799a72ace1c75f809937c245d9821
nishantyaji <nishantyaji@users.noreply.github.com>
2024-12-31 22:28:34 +0530

    Code to calculate multiplicative inverse (mod) and power (mod)

---
+++ PowMod.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<stdbool.h>
+#include <math.h>
+
+int * binaryRepresentation(int num, bool isZeroIndexLSB, int * resSize) {
+	int size = floor(log(num) / log(2)) + 1;
+	* resSize = size;
+	int * res = (int *) malloc(sizeof(int) * size);
+	int index = isZeroIndexLSB ? 0: size - 1;
+
+	while(num > 0) {
+		int rem = num & 1;
+		res[index] = rem;
+		index = isZeroIndexLSB ? index + 1: index - 1;
+		num = num >> 1;
+	}
+	return res;
+}
+
+long powGeneral(int a, int pw, int base) {
+	long long res = 1, num = a;
+	int binSize = 0, i = 0;
+	int * bin = binaryRepresentation(pw, true, &binSize);
+	for(i = 0; i < binSize; i++) {
+		if(bin[i] > 0) {
+			res = (res * num) % base;
+		}
+		num = (num * num) % base;
+	}
+	return res;
+}
+
+long multiplicativeInverse(int a, int base) {
+	return powGeneral(a, base - 2, base);
+}
+
+long long powMod(int a, int pw, int base) {
+	int num = a;
+	if(pw < 0) {
+		num = multiplicativeInverse(a, base);
+		pw = -pw;
+	}
+	return powGeneral(num, pw, base);
+}
+
+int main()
+{
+	printf("Hello World");
+	printf("\n%lld", powMod(2, 100000, 1000000007));
+	printf("\n%lld", powMod(2, -100000, 1000000007));
+	return 0;
+}
